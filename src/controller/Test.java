7d42dfbd52bf8b2f32a8dd934524d14f74b71328
Wasabi974 <33418382+Wasabi974@users.noreply.github.com>
2020-07-16 09:26:20 +0200

    Test

--- src/controller/Test.java
+++ src/controller/Test.java
@@ -16,7 +16,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		AutoEntreprise auto = new AutoEntreprise("1234 5678 9755 9865", "BNP");
-		Sas sas = new Sas("98765 43276 89986786", "Renault", "13 rue Maryse Hilsz");
+		Sas sas = new Sas("98765 43276 89986786", "Renault", "42 rue du Test");
 		Entreprise fake = new Entreprise("76575", "Faker", TypeEntreprise.INCONNU);
 		
 		try {
